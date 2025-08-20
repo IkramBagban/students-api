1ae22e9f9647058c29a48c6072a0fa8fd19f599e
ikramBagban <107988060+IkramBagban@users.noreply.github.com>
2025-08-20 13:51:51 +0530

    add get student and post student endpoints

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -1,7 +1,59 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type Student struct {
+	ID   uint   `json:"id" gorm:"primaryKey"`
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+}
+
+var db *gorm.DB
 
 func main() {
 	fmt.Println("Hello, World!")
+
+	var err error
+
+	dsn := "host=localhost user=postgres password=pass123 dbname=postgres port=5432 sslmode=disable TimeZone=Asia/Kolkata"
+	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database") // this will stop the application
+	}
+	db.AutoMigrate(&Student{}) // this will create the students table in the database
+
+	r := gin.Default() // this will create a new gin router, meaning we can define our API routes
+
+	// routes
+	r.GET("/students", getStudents)
+	r.POST("/student", createStudent)
+
+	r.Run(":8000")
+}
+
+func getStudents(c *gin.Context) {
+	var students []Student
+	db.Find(&students)
+	c.JSON(http.StatusOK, students)
+}
+
+func createStudent(c *gin.Context) {
+	var student Student
+
+	if err := c.ShouldBindJSON(&student); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+
+		return
+
+	}
+	db.Create(&student)
+	c.JSON(http.StatusCreated, student)
+
 }
